b3626aa8c569bfb89c6e03091de56c36c7a5733f
pauloricardorc <pauloricardorc0@gmail.com>
2020-05-31 23:47:21 -0400

    edit sever

--- server.js
+++ server.js
@@ -6,24 +6,30 @@ app.use(express.json())
 
 app.get('/apiPerguntas', function(req, res){
 
-    res.json(data.map(re => re.Pergunta))
-
+    res.json(data)
 })
 
-app.get('/apiPerguntas/pergunta/:id', function(req, res){
-    const { id } = req.params
 
-    const result = data.map(res => res.Pergunta[id])
+// app.get('/apiPerguntas/pergunta/:id', function(req, res){
+//     const { id } = req.params
+    
+//     const resdata = data.Pergunta()
 
-    res.json(result)
+//     res.json(resdata)
+// })
+
+app.get('/apiPerguntas/pergunt', function(req, res){
+    
+
+    res.json(data.Pergunta)
 })
 
 app.get('/apiPerguntas/perguntas', function(req, res){
+    const respe = data.Pergunta.map(map => map.pergunta)
 
-    const cliente  = data.find(cli => cli.Pergunta)
-
-    res.json(cliente.Pergunta.map(rep => rep.pergunta))
+    res.json(respe)
 })
+
 
 app.get('/apiPerguntas/questao/:id', function(req, res){
     const { id }  = req.params
