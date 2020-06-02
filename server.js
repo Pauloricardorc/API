94ce9b3d8bcaa7ba90ae740ad622c0b2c979081b
pauloricardorc <pauloricardorc0@gmail.com>
2020-06-01 23:01:43 -0400

    fez arte
    Co-authored-by: Maxwell M.Silva <maxwellmachadom.m.s@gmail.com>

--- server.js
+++ server.js
@@ -8,33 +8,27 @@ app.get('/apiPerguntas', function(req, res){
     res.json(data)
 })
 
-
-// app.get('/apiPerguntas/pergunta/:id', function(req, res){
-//     const { id } = req.params
-    
-//     const resdata = data.Pergunta()
-
-//     res.json(resdata)
-// })
-
-app.get('/apiPerguntas/pergunt', function(req, res){
-    const params = req.query;
-    const respques = data.Pergunta.map(se => se.value == params)
-    res.json(respques)
-})
-
-app.get('/apiPerguntas/perguntas', function(req, res){
-    const respe = data.Pergunta.map(map => map.tema)
-
-    res.json(respe)
-})
-
-
 app.get('/apiPerguntas/questao/', function(req, res){
-    const cliente = data.Pergunta.map(te => te.questao)
-
-    res.json(cliente)
+    const client = data.Quiz.map(ma => ma.PrimeiraTela.map(pe => pe.Questão))
+    
+    res.json(client)
 })
 
+app.get('/apiPerguntas/questao/:id', function(req, res){
+    const { id } = req.params
+    const client = data.Quiz.map(ma => ma.PrimeiraTela.map(pe => pe.Questão[id]))
+    // console.log(client[id])
+    res.json(client)
+})
 
 app.listen(process.env.PORT || 3000)
+
+
+
+    // app.get('/apiPerguntas/pergunta/:id', function(req, res){
+    //     const { id } = req.params
+        
+    //     const resdata = data.Pergunta()
+    
+    //     res.json(resdata)
+    // })
